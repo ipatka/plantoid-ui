d9824065ed5cc85dc78b3c394ce9efb85cbf2a14
yaoe <pdefilippi@gmail.com>
2024-01-06 04:59:30 +0100

    gallery with infinite scrolling

--- packages/react-app/src/components/Gallery-backup.jsx
+++ packages/react-app/src/components/Gallery-backup.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import InfiniteScroll from "react-infinite-scroll-component";
-
 // import { LazyLoadComponent } from 'react-lazy-load-image-component';
-
 
 
 import _ from "lodash";
@@ -57,13 +54,6 @@ export default function Gallery({
   //       // }
   // }];
 
-  // INFINITE SCROLL STUFF
-  const [items, setItems] = useState([]);
-  const [mounted, setMounted] = useState(false)
-  const [hasMore, setHasMore] = useState(true);
-  const [index, setIndex] = useState(10);
-
-
   if (address) {
     console.log("address is ....", address);
     console.log("GRAPH DATA (gallery) !!!!!!!! ", graphData);
@@ -75,8 +65,6 @@ export default function Gallery({
   // const contractAddress = "0x4073E38f71b2612580E9e381031B0c38B3B4C27E";
   // const contractAddress2 = "0x0B60EE161d7b67fa231e9565dAFF65b34553bC6F";
   const [nfts, setNfts] = useState([]);
-
-  useEffect(() => { setMounted(true); }, [])
 
   useEffect(() => {
     const fetchNFTs = async () => {
@@ -95,53 +83,19 @@ export default function Gallery({
       });
 
       setNfts(tempNfts);
-
-      // INFINITE SCROLL STUFF
-      setItems(tempNfts.slice(0, index));
-
-      console.log("-----------------***--------------------", tempNfts);
-      console.log("-------------------------------------", items);
-
     };
 
     fetchNFTs();
   }, [graphData]);
 
-
-      // INFINITE SCROLL STUFF
-    const fetchMoreData = () => {
-      //setItems((nfts.slice(0, index)));
-      const newItems = Array.from(nfts.slice(index, index+10))
-      setItems(prevItems => [...prevItems, ...newItems])
-      setIndex((prevIndex => prevIndex + 10));
-      items.length < nfts.length ? setHasMore(true) : setHasMore(false);
-      console.log("lennnnnnnn ===== \n"); 
-      console.log(items.length < nfts.length);
-      console.log("fetching more..........." + items.length + "    ........" + items);
-    }
-
-
-
   return (
-
-
     <div>
       <h1 className="gallery-title">Welcome to the gallery of Plantoid 15</h1>
       <p className="gallery-description">Here you can see all of its digital NFT seeds</p>
       
-     
-      <InfiniteScroll
-          dataLength={items.length}
-          next={fetchMoreData}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-          >
+      {/* <LazyLoadComponent> */}
       
       <div className="nft-gallery">
-
-   
-
-
           {/* { [].concat(nfts)
           .sort((a, b) => a[0] > b[0] ? 1 : - 1)
           .map((nft, index) => (
@@ -153,26 +107,12 @@ export default function Gallery({
         ))} */}
 
 
-        {/* THIS WORKS WELL */}
-        {/* {nfts.sort((a, b) => parseInt(a[0]) > parseInt(b[0]) ? -1 : 1).map((nft, index) => (
+        {nfts.sort((a, b) => parseInt(a[0]) > parseInt(b[0]) ? -1 : 1).map((nft, index) => (
           <div key={index} className="image-container">
             <video src={nft[1]} alt="NFT" className="image" controls loop />
             <p>Seed #{nft[0]}</p>
           </div>
-        ))} */}
-
-        {/* THIS IS FOR THE INFINITE SCROLL */}
-        
-        {items.sort((a, b) => parseInt(a[0]) > parseInt(b[0]) ? -1 : 1).map((nft, index) => (
-          <div key={index} className="image-container" style={{"height": 200, "width": 200 }}>
-            <video src={nft[1]} alt="NFT" className="image" controls loop />
-            <p>Seed #{nft[0]}</p>
-          </div>
-        ))} 
-       
-
-
-
+        ))}
 
         {/* {nfts.map((nft, index) => (
           <div key={index} className="image-container">
@@ -184,11 +124,8 @@ export default function Gallery({
 
       </div>
 
-      </InfiniteScroll>
-
-
+      {/* </LazyLoadComponent> */}
 
     </div>
-
   );
 }
